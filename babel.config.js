b2d02d9fd9c2007990a70ac5e67cee7fb4fd6dd8
nianzu.xu@trinasolar.com <nianzu.xu@trinasolar.com>
2019-10-11 09:05:40 +0800

    #

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,21 @@
 module.exports = function (api) {
-//   const { BABEL_MODULE, NODE_ENV } = process.env;
-//   const useESModules = BABEL_MODULE !== 'commonjs' && NODE_ENV !== 'test';
+  // const { BABEL_MODULE, NODE_ENV } = process.env;
+  // const useESModules = BABEL_MODULE !== 'commonjs' && NODE_ENV !== 'test';
 
   api && api.cache(false);
 
   return {
     presets: [
       [
-        '@babel/preset-env',
+        "@babel/preset-env",
         {
           loose: true,
-        //   modules: useESModules ? false : 'commonjs'
+          // modules: useESModules ? false : 'commonjs'
           modules: false
         }
       ],
       [
-        '@vue/babel-preset-jsx',
+        "@vue/babel-preset-jsx",
         {
           functional: false
         }
@@ -23,12 +23,12 @@ module.exports = function (api) {
     ],
     plugins: [
       [
-        '@babel/plugin-transform-runtime',
+        "@babel/plugin-transform-runtime",
         {
           corejs: false,
           helpers: true,
           regenerator: false,
-          useESModules
+          useESModules: false
         }
       ],
       '@babel/plugin-transform-object-assign'
