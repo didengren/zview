5fc810a4701e04325b9f744ee340a36c939f5cb1
nianzu.xu@trinasolar.com <nianzu.xu@trinasolar.com>
2020-05-12 15:06:35 +0800

    #

--- common/tools/index.js
+++ common/tools/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 
-// 挖掘深层次文件
+// 判断dir是否是文件夹
 exports.isDirectory = (originPath, dir) => {
   const dirs = fs.readdirSync(originPath);
   return dirs.some((item) => item === dir);
